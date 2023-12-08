ee9a7ddba647d35e8be5990135f654c873b6a73f
Wandile Mawelela <wandile1997@gmail.com>
2023-12-08 11:12:28 +0200

    [ADD/FIX/REF/DEL] Refactoring and Debugging

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 import json
 from models.base_model import BaseModel
--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+import os # Debug
 from models import storage
 from models.base_model import BaseModel
 
+print("Current working directory:", os.getcwd()) # Debug
 all_objs = storage.all()
 print("-- Reloaded objects --")
 for obj_id in all_objs.keys():
@@ -14,4 +16,4 @@ my_model.name = "My_First_Model"
 my_model.my_number = 89
 my_model.save()
 print(my_model)
-
+print("File saved to:", storage._FileStorage__file_path) # Debug
